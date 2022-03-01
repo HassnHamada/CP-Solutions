009362d3e9b6f1384fd2a8eb1843f0176269856a
HassnHamada <hassneltokhy4@gmail.com>
2022-03-01 20:42:45 +0200

    1/3/2022

--- 1642D.py
+++ 1642D.py
@@ -2,34 +2,35 @@ import sys
 from collections import Counter
 
 
+def magic(arr, n, ans_q: list, ex):
+    stk = list()
+    for i in range(1, n):
+        if len(stk) != 0 and stk[-1] == arr[i]:
+            stk.pop()
+        else:
+            ans_q.append((ex + n + i, arr[i]))
+            stk.append(arr[i])
+    return stk[::-1] + arr[n+1:]
+
+
 def run():
-    n = int(input())
+    _ = int(input())
     arr = list(map(int, input().split()))
     counter = Counter(arr)
     if any(i % 2 == 1 for i in counter.values()):
         print(-1)
     else:
-        ext, stk, ans = list(), list(), list()
-        for i in range(n):
-            if len(stk) != 0 and stk[-1] == arr[i]:
-                ext.append(stk.pop())
-            else:
-                ans.append((n + len(ext), arr[i]))
-                ext.append(arr[i])
-                stk.append(arr[i])
-            print(ext)
-            print(stk)
-        assert len(stk) % 2 == 0
-        assert all(stk[i] == stk[len(stk)//2+i] for i in range(len(stk) // 2))
-        assert len(ext) + len(stk) == 2 * len(ans)
-        print(len(ans))
-        print(*(f"{i} {j}"for i, j in ans), sep='\n')
-        if len(stk) == 0:
-            print(1)
-            print(n + len(ext))
-        else:
-            print(2)
-            print(n + len(ext), len(stk))
+        ex = 0
+        ans_q, ans_l = list(), list()
+        while len(arr) != 0:
+            ans_l.append(arr.index(arr[0], 1))
+            arr = magic(arr, ans_l[-1], ans_q, ex)
+            ex += 2 * ans_l[-1]
+        print(len(ans_q))
+        if len(ans_q) != 0:
+            print(*(f"{i} {j}" for i, j in ans_q), sep='\n')
+        print(len(ans_l))
+        print(*(2*i for i in ans_l))
 
 
 def main():

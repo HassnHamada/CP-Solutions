92605ee2f5d3e097b88bf6944f09e7841bcb7255
Hassn Hamada <hassneltokhy4@gmail.com>
2023-08-13 22:39:59 +0300

    .

---
+++ 1844A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int N = 1e5 + 10, M = 3, L = 30, MOD = 1e9 + 7, HV = 151, INF = 0x3f3f3f3f;
+
+void run()
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d\n", a + b);
+}
+
+int main()
+{
+    // freopen("_output.txt", "w", stdout);
+    // freopen("_input.txt", "r", stdin);
+    int t = 1;
+    scanf("%d", &t);
+    while (t--)
+    // while (scanf("%d", &n), n)
+    {
+        run();
+    }
+    return 0;
+}
